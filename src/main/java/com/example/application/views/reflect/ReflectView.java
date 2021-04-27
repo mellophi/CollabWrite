210fb45b0792f3eef6c7849da74b24e94445c228
Parijat <parijatmoulik11@gmail.com>
2021-04-27 19:47:54 +0530

    Added basic card instead of placeholder in reflect

--- src/main/java/com/example/application/views/reflect/ReflectView.java
+++ src/main/java/com/example/application/views/reflect/ReflectView.java
@@ -1,5 +1,7 @@
 package com.example.application.views.reflect;
 
+import com.github.appreciated.card.Card;
+import com.github.appreciated.card.content.Item;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
@@ -14,7 +16,9 @@ public class ReflectView extends Div {
 
     public ReflectView() {
         addClassName("reflect-view");
-        add(new Text("Content placeholder"));
+//        add(new Text("Content placeholder"));
+        Card card = new Card(new Item("Mello", "Hello"));
+        add(card);
     }
 
 }
