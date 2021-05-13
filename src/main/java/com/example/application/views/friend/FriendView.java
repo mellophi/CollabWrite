5b206410c3e6b095c77f801bc75b45fc14102345
anjishnucha395 <anjishnucha395@gmail.com>
2021-05-13 14:19:15 +0530

    Test

--- src/main/java/com/example/application/views/friend/FriendView.java
+++ src/main/java/com/example/application/views/friend/FriendView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "friend", layout = MainView.class)
+//cdfdsf
 @PageTitle("Friend")
 public class FriendView extends VerticalLayout {
 }
